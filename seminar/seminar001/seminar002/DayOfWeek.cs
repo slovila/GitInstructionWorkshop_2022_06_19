a884938986326ed34431717cad3c989e79b27aad
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-06-16 23:32:46 +0300

    seminar004_n_N

--- seminar/seminar001/seminar002/DayOfWeek.cs
+++ seminar/seminar001/seminar002/DayOfWeek.cs
@@ -1,7 +1,7 @@
-﻿string[] array={"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+﻿string[] array = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
 Console.WriteLine("Введите номер дня недели ");
 int numberOfWeek = Convert.ToInt32(Console.ReadLine());
-if(numberOfWeek > 7) Console.WriteLine("Дня недели с таким порядковым номером не существует!");
+if (numberOfWeek > 7) Console.WriteLine("Дня недели с таким порядковым номером не существует!");
 int index = numberOfWeek - 1;
 Console.Write("Этот день недели - ");
 Console.WriteLine(array[index]);
---
+++ seminar/seminar004_n_N/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while (count <= number)
+{
+    Console.WriteLine(count);
+    count++;
+}
+
+
