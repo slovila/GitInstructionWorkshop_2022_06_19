0d9073ccdb12b0c08584ac052dfbb0a682d49467
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-06-17 12:28:26 +0300

    seminar006_Dop_Obratnoe

---
+++ seminar/seminar006_Dop_Obratnoe/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+double revNumber = 1 / number;
+Console.Write("Обратное число будет равно: ");
+Console.Write(revNumber);
+
