4bacc02a38dd89e55febcd74585779bf569073dc
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-06-18 18:33:19 +0300

    DZ_001 modify

--- seminar/HomeWorkSeminar/DZ_001/Program.cs
+++ seminar/HomeWorkSeminar/DZ_001/Program.cs
@@ -5,15 +5,18 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter second number: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-if (numberA > numberB)
-{
-    Console.Write("maximal number is: " + numberA);
-}
-if (numberA < numberB)
-{
-    Console.Write("Maximal number is: " + numberB);
-}
+
 if (numberA == numberB)
+
 {
     Console.Write("*** The numbers are equal ***");
+}
+
+if (numberA > numberB)
+{
+    Console.Write(" Maximal number is: " + numberA + " Minimal number is: " + numberB);
+}
+else
+{
+    Console.Write(" Maximal number is: " + numberB + " Minimal number is: " + numberA);
 }
