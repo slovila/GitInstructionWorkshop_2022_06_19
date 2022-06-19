d8fcbfd400997097a1fd47f4f505823a5e67fd2d
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-06-19 14:49:02 +0300

    zal_7

--- seminar/seminar002_DayOfWeek/DayOfWeek.cs
+++ seminar/seminar002_DayOfWeek/DayOfWeek.cs
@@ -1,5 +1,6 @@
 ﻿string[] array = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
-Console.WriteLine("Введите номер дня недели ");
+
+/*Console.WriteLine("Введите номер дня недели ");
 int numberOfWeek = Convert.ToInt32(Console.ReadLine());
 if (numberOfWeek > 7) Console.WriteLine("Дня недели с таким порядковым номером не существует!");
 int count = 0;
@@ -13,5 +14,16 @@ while (count < array.Length)
     Console.WriteLine(array[count] + "  " + array[count].Length + "   символов");
     count++;
 
+}*/
+
+int count = 0;
+while (count < array.Length)
+{
+    if (array[count] == "п" || array[count] == "с")
+    {
+        Console.WriteLine($"Day Of Week {array[count]}" );
+        count++;
+    }
 }
-Console.WriteLine(array[0] + array[4] + array[2] + array[5]);
+
+// Console.WriteLine(array[0] + array[4] + array[2] + array[5]);
